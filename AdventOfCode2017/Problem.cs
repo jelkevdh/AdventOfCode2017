43c730660af52309aec51096c9967ccb50938fba
VluggeJapie <jelkevdhorst@gmail.com>
2017-12-05 17:12:22 +0100

    Day1

---
+++ AdventOfCode2017/Problem.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace AdventOfCode2017 {
+  public abstract class Problem {
+    /// <summary>
+    /// The name of this problem.
+    /// </summary>
+    public string Name { get; }
+
+    /// <summary>
+    /// The default input.
+    /// </summary>
+    public string DefaultInput { get; }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Problem"/> class.
+    /// </summary>
+    /// <param name="name">The name of this problem.</param>
+    /// <param name="defaultInput">The default input.</param>
+    public Problem(string name, string defaultInput) {
+      Name = name;
+      DefaultInput = defaultInput;
+    }
+
+    /// <summary>
+    /// Reads the puzzle input.
+    /// </summary>
+    /// <returns>The puzzle input.</returns>
+    public virtual string ReadInput() {
+      string input = String.Empty;
+
+      do {
+        Console.WriteLine($"-=[ {Name} ]=-");
+        Console.WriteLine($"Enter sequence [{DefaultInput}]:");
+        input = Console.ReadLine();
+
+        if (input == "") {
+          input = DefaultInput;
+        }
+
+      } while (!IsInputValid(input));
+
+      return input;
+    }
+
+    /// <summary>
+    /// Returns a value indicating whether the specified input is valid.
+    /// </summary>
+    /// <param name="input">The input to check.</param>
+    /// <returns><c>true</c> if valid and <c>false</c> otherwise.</returns>
+    public virtual bool IsInputValid(string input) {
+      return !String.IsNullOrWhiteSpace(input);
+    }
+  }
+}
---
+++ AdventOfCode2017/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace AdventOfCode2017 {
+  class Program {
+    static void Main(string[] args) {
+      var problem = new Day1.Problem2();
+
+      string input = problem.ReadInput();
+
+      try {
+        problem.Solve(input);
+      }
+      catch (Exception e) {
+        Console.WriteLine("Error solving the problem: " + e.Message);
+      }
+
+      ShowPressEnterToExit();
+    }
+
+    /// <summary>
+    /// Shows the press-enter-to-exit message and waits for the user to press enter.
+    /// </summary>
+    static void ShowPressEnterToExit() {
+      Console.WriteLine(String.Empty);
+      Console.WriteLine("Press [enter] to exit");
+      Console.ReadLine();
+    }
+  }
+}
